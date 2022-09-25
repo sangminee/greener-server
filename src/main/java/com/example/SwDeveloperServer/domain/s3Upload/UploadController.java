33227985c5a96ff867d9b55dd02e8a5d044a1010
sangminee <maybesm10@gmail.com>
2022-09-26 00:38:30 +0900

    S3 사용예시

---
+++ src/main/java/com/example/SwDeveloperServer/domain/s3Upload/UploadController.java
@@ -0,0 +1,24 @@
+package com.example.SwDeveloperServer.domain.s3Upload;
+
+import com.example.SwDeveloperServer.utils.response.ResponseService;
+import com.example.SwDeveloperServer.utils.response.SuccessStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class UploadController {
+    private final S3UploadService s3UploadService;
+    private final ResponseService responseService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        String file = s3UploadService.upload(multipartFile);
+        return responseService.successResult(file, SuccessStatus.SUCCESS);
+    }
+
+}
