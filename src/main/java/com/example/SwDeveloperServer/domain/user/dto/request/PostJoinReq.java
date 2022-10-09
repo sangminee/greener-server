463b14be3b10956881e649c6db7f9e8f0fc4a2f5
sangminee <maybesm10@gmail.com>
2022-10-09 14:37:05 +0900

    Refactor : User state dataType 변경

--- src/main/java/com/example/SwDeveloperServer/domain/user/dto/request/PostJoinReq.java
+++ src/main/java/com/example/SwDeveloperServer/domain/user/dto/request/PostJoinReq.java
@@ -1,15 +1,19 @@
 package com.example.SwDeveloperServer.domain.user.dto.request;
 
+import com.example.SwDeveloperServer.domain.user.entity.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @ApiModel(description = "회원가입")
 @Setter
 public class PostJoinReq {
 
+    @NotBlank(message = "email is not null")
     @ApiModelProperty(notes = "이메일", example="test000")
     private String email;
     @ApiModelProperty(notes = "비밀번호", example="test000")
@@ -27,8 +31,36 @@ public class PostJoinReq {
     private int userServiceAgreement;
     @ApiModelProperty(notes = "휴대폰 인증 유무 - 0 : 인증 o,  1 : 인증 x", example="0")
     private int phoneAgreement;
-    @ApiModelProperty(notes = "유저 타입 - 0 : 회원, 1 : 관리자", example="0")
-    private int userType;
-    @ApiModelProperty(notes = "유저 상태 - 0:활성, 1:비활성", example="0")
-    private int state;
+
+    @ApiModelProperty(notes = "유저 타입 - USER, ADMIN", example="USER")
+    private String userType;
+
+    public PostJoinReq(String email, String password, String name,
+                       String nickname, String userPhotoUrl, String phone,
+                       int userServiceAgreement, int phoneAgreement,
+                       String userType) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.userPhotoUrl = userPhotoUrl;
+        this.phone = phone;
+        this.userServiceAgreement = userServiceAgreement;
+        this.phoneAgreement = phoneAgreement;
+        this.userType = userType;
+    }
+
+    public User toEntity(){
+        return User.builder()
+                .email(this.email)
+                .password(this.password)
+                .name(this.name)
+                .nickname(this.nickname)
+                .userPhotoUrl(this.userPhotoUrl)
+                .phone(this.phone)
+                .userServiceAgreement(this.userServiceAgreement)
+                .phoneAgreement(this.phoneAgreement)
+                .userType(this.userType)
+                .build();
+    }
 }
